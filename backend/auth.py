c24dda89334a8205a7e603b65595f0bfaca2c8a3
Matthew Carroll <60335800+mmc102@users.noreply.github.com>
2024-12-03 15:37:12 -0800

    remove jwt temp secret

--- backend/auth.py
+++ backend/auth.py
@@ -1,9 +1,16 @@
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from passlib.context import CryptContext
+from dotenv import load_dotenv
+import os
 
-# Secret key and algorithm for JWT
-SECRET_KEY = "your_secret_key_here"
+load_dotenv()
+
+# Fetch the database URL from the environment
+JWT_SECRET = os.getenv("JWT_SECRET")
+
+assert JWT_SECRET is not None
+
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60  * 24 * 365 # 1 year yolo
 
@@ -25,13 +32,13 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None) -> s
         expire = datetime.utcnow() + expires_delta
     else:
         expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({"exp": expire})
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+        to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, JWT_SECRET, algorithm=ALGORITHM)
 
 # Verify a JWT token
 def decode_access_token(token: str):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(token, JWT_SECRET, algorithms=[ALGORITHM])
         return payload
     except JWTError:
         return None
